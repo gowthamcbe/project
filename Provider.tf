78e5e0557faf526e6eed45919b2a72dd3743a29d
gowthamcbe <148002853+gowthamcbe@users.noreply.github.com>
2023-10-19 14:42:51 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,14 +1,21 @@
-terraform {
-  required_providers {
+required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.17.0"
     }
   }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "s3-statefile-terraform"
+    key    = "zomoto/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
 }
 
 
 provider "aws" {
   region = var.aws_region
 }
+
 
